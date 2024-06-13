f4135bcfdfcb0a76c0445bdec545c5a476a4466b
KESHAV <118046987+jadavkeshav@users.noreply.github.com>
2024-06-13 20:21:51 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,10 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  credentials: true,
+  origin: process.env.CLIENT_URL,
+}));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
