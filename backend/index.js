78070d96803bb8a078761dd27becfe11d6b11cf2
KESHAV <118046987+jadavkeshav@users.noreply.github.com>
2024-06-13 20:19:39 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,10 +10,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({
-  credentials: true,
-  origin: "https://nyaaysahayak.netlify.app/",
-}));
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
