3af0aef92a0f61527439a1972175d5490f952344
jadavkeshav <jadavkeshav2005@gmail.com>
2024-12-27 14:47:38 +0530

    url

--- backend/index.js
+++ backend/index.js
@@ -12,8 +12,8 @@ const app = express();
 
 app.use(cors({
   credentials: true,
-  // origin: 'https://nyaay-sahayak.vercel.app',
-  origin: 'http://localhost:3000',
+  origin: 'https://nyaay-sahayak.vercel.app',
+  // origin: 'http://localhost:3000',
   
 }));
 
